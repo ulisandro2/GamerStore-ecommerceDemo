c8f207ece553dc203a967cd07a310050e7743b90
ulises <sanchezulises952@gmail.com>
2022-06-28 19:10:14 -0300

    cambios

--- app-sanchez/src/Containers/ItemDetailContainers/ItemDetailContainer.jsx
+++ app-sanchez/src/Containers/ItemDetailContainers/ItemDetailContainer.jsx
@@ -9,41 +9,18 @@ import { getFetchOne } from '../../helpers/getFetch'
 const ItemDetailContainer = () => {
     const [producto , setProductos] = useState({});
     
-     const id= 1
-    const {} = useParams()
+     
+     const {id} =useParams()
 
        useEffect(() =>{
         getFetchOne(id)
         .then((resp)=>  setProductos(resp))
         .catch(err => console.log(err))
        },[])
-    
-     
-            // <div className='col-md-4 p-1'
-            //             key={detail.id}
-            //            >
-            //                <div className='card w-100 mt-5'>
-            //                    <div className='card-header'>
-            //                        { `${detail.categoria}`}
-            //                    </div>
-            //                    <div className='card-body'>
-            //                        {detail.stock}
-            //                    </div>
-            //                    <div className='card-footer'>
-            //                        <button className='btn btn-outline-primary btn-block'>
-            //                            Detalle del producto
-            //                        </button>
-            //                    </div>
-            //                </div>
-            //            </div>
-        
        
   
     return <ItemDetail producto={producto} />
-    // <div>
-    //     <h2>Numero de Producto: {detail.id}</h2>
-    //     <h2>Su categoria es : {detail.categoria}</h2>
-    // </div>
+    
   
 }
 
